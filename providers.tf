3aa088e0dc3f081006b2cd7d9b95fcb31dffce77
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2019-07-02 11:02:29 +0300

    code update

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
+  region     = "${var.vpn_gw_aws_region}"
 
   version = "< 2.0.0"
 }
